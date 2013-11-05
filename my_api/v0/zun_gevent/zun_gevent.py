ededb115dc121010a8b3573c68ec3bc2450d2d1f
nbari <nbari@dalmp.com>
2013-11-05 21:30:00 +0000

    modified:   my_api/v0/zun_gevent/zun_gevent.py

--- my_api/v0/zun_gevent/zun_gevent.py
+++ my_api/v0/zun_gevent/zun_gevent.py
@@ -1,9 +1,10 @@
 """
 default resource
 """
-import json
 import gevent
 import gevent.socket
+import json
+import logging
 from zunzuncito import http_status_codes
 from zunzuncito.tools import MethodException, HTTPException, allow_methods
 
@@ -24,10 +25,23 @@ class APIResource(object):
     def __init__(self, api):
         self.api = api
         self.status = 200
-        self.headers = {}
-        print '---xx---'
+        self.headers = api.headers.copy()
+        self.log = logging.getLogger()
+        # self.log.setLevel('DEBUG')
+        self.log.setLevel('INFO')
+        self.log = logging.LoggerAdapter(
+            logging.getLogger(), {
+                'rid': api.request_id,
+                'indent': 4
+            })
+        self.log.info(dict((x, y) for x, y in (
+            ('API', api.version),
+            ('URI', api.URI),
+            ('method', api.method)
+        )))
 
-    def dispatch(self):
+
+    def dispatch(self, environ, start_response):
         t = gevent.spawn(long_task)
         t.join()
         yield "sleeping for 3 seconds...<br/>"
