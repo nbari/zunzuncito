da257a0dabc53c7fcb0ce339d20be52d07450e7a
nbari <nbari@dalmp.com>
2013-11-06 09:54:37 +0000

    modified:   app.py

--- app.py
+++ app.py
@@ -1,12 +1,29 @@
-# uwsgi --http :8080 --wsgi-file app.py --callable app --master
+"""
+To clean:
+find . -iname "*.pyc" -exec rm -f {} \;
 
-import os
+To run:
+uwsgi --http :8080 --wsgi-file app.py --callable app --master
+"""
+
 import zunzuncito
 
+"""Root
+Name of the directory containing the API
+"""
 root = 'my_api'
 
+"""Versions
+Supported versions
+"""
 versions = ['v0', 'v1', 'v2']
 
+"""Route format:
+    :URI: a regex matching the URI by default
+    :handler: the python module that will handle the request
+    :methods: list of allowed methods (comma separated)
+        if none all are accepted
+"""
 routes = [
     # ('/.*', 'default'),
     ('/teste', 'test_get', 'GET'),
