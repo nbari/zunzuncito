c06804677e15ec70987d5830f1f884fabf55b85c
nbari <nbari@dalmp.com>
2013-11-09 22:28:59 +0000

    modified:   my_api/v0/zun_default/zun_default.py 	new file:   my_api/v0/zun_default/zun_ip_tools.py 	new file:   my_api/v0/zun_http_status/__init__.py 	new file:   my_api/v0/zun_http_status/zun_http_status.py 	new file:   my_api/v0/zun_ip_tools/__init__.py 	new file:   my_api/v0/zun_ip_tools/zun_ip_tools.py

--- my_api/v0/zun_http_status/zun_http_status.py
+++ my_api/v0/zun_http_status/zun_http_status.py
@@ -33,11 +33,14 @@ class APIResource(object):
             getattr(http_status_codes, 'HTTP_%d' %
                     self.status), list(headers.items()))
         data = {}
-        data['about'] = ("Hi %s, I am zunzuncito a micro-framework for creating"
-                         " REST API's, you can read more about me in: "
-                         "www.zunzun.io") % environ.get('REMOTE_ADDR', 0)
-        data['request-id'] = self.api.request_id
+        data['API'] = self.api.version
+        data['REMOTE_ADDR'] = environ.get('REMOTE_ADDR', 0)
         data['URI'] = self.api.URI
         data['method'] = self.api.method
+        data['HTTP_X_APPENGINE_CITY'] = environ.get('HTTP_X_APPENGINE_CITY', 0)
+        data['HTTP_X_APPENGINE_CITYLATLONG'] = environ.get(
+            'HTTP_X_APPENGINE_CITYLATLONG', 0)
+        data['HTTP_X_APPENGINE_COUNTRY'] = environ.get(
+            'HTTP_X_APPENGINE_COUNTRY', 0)
 
-        return json.dumps(data, sort_keys=True, indent=4)
+        return json.dumps(data, sort_key=True, indent=4)
