bb022a84c4c76773cf8a9cb6933473b6a511441c
nbari <nbari@dalmp.com>
2013-12-12 10:03:09 +0000

    modified:   zun_http_status.py

--- my_api/default/v0/zun_http_status/zun_http_status.py
+++ my_api/default/v0/zun_http_status/zun_http_status.py
@@ -14,17 +14,6 @@ class APIResource(object):
         self.status = 200
         self.headers = api.headers.copy()
         self.log = logging.getLogger()
-        self.log.setLevel('INFO')
-        self.log = logging.LoggerAdapter(
-            logging.getLogger(), {
-                'rid': api.request_id,
-                'indent': 4
-            })
-        self.log.info(dict((x, y) for x, y in (
-            ('API', api.version),
-            ('URI', api.URI),
-            ('method', api.method)
-        )))
 
     @allow_methods('get')
     def dispatch(self, environ, start_response):
