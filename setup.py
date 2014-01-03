12a00da911f9438ccb8292d9896a781404d09bf2
nbari <nbari@dalmp.com>
2014-01-03 16:08:06 +0000

    deleted:    MANIFEST.in 	modified:   setup.py 	modified:   zunzuncito.egg-info/PKG-INFO 	modified:   zunzuncito.egg-info/SOURCES.txt

--- setup.py
+++ setup.py
@@ -20,7 +20,6 @@ setup(
     keywords='wsgi web api framework rest http',
     url='https://github.com/nbari/zunzuncito',
     packages=['zunzuncito'],
-    long_description=open('README.rst', 'r').read(),
     classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Console',
