387ce727b4edccc46d9442e56bddbf63c525b0f4
nbari <nbari@dalmp.com>
2014-01-06 17:37:06 +0000

    new file:   my_api/default/v0/zun_headers/__init__.py 	new file:   my_api/default/v0/zun_headers/zun_headers.py

---
+++ my_api/default/v0/zun_headers/zun_headers.py
@@ -0,0 +1,32 @@
+"""
+default resource
+"""
+import logging
+import uuid
+from zunzuncito import http_status_codes
+from zunzuncito import tools
+
+
+class APIResource(object):
+
+    def __init__(self, api):
+        self.api = api
+        self.status = 200
+        self.headers = api.headers.copy()
+        self.log = logging.getLogger()
+        self.log.info(tools.log_json({
+            'vroot': api.vroot,
+            'API': api.version,
+            'URI': api.URI,
+            'method': api.method
+        }, True)
+        )
+
+    def dispatch(self, environ, start_response):
+        headers = self.api.headers
+        start_response(
+            getattr(http_status_codes, 'HTTP_%d' %
+                    self.status), list(headers.items()))
+        headers['naranjas'] = str(uuid.uuid4())
+
+        return tools.log_json(headers, 4)
