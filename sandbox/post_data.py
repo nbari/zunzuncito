12a0f65c8a64c046683f41af007f0c4c5036819c
nbari <nbari@dalmp.com>
2014-01-26 21:26:35 +0000

    new file:   post_data.py

---
+++ sandbox/post_data.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+nc -l 8080
+"""
+import requests
+
+requests.post('http://localhost:8080', data={u'post': 'niño coraçao'})
+
+# import urllib
+# s = urllib.unquote_plus(b"ni%C3%B1o+cora%C3%A7ao").decode('utf-8')
