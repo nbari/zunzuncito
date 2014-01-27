79840eb08423ffbd8ec82202278f5e237b558bb7
nbari <nbari@dalmp.com>
2014-01-27 08:31:28 +0000

    modified:   my_api/default/v0/zun_unicode/zun_unicode.py 	modified:   sandbox/post_data.py

--- my_api/default/v0/zun_unicode/zun_unicode.py
+++ my_api/default/v0/zun_unicode/zun_unicode.py
@@ -22,4 +22,20 @@ class APIResource(object):
     @tools.allow_methods('get, post')
     def dispatch(self, environ):
 
-        return tools.log_json(environ, 4)
+        body = ''  # b'' for consistency on Python 3.0
+
+        try:
+            length = int(environ.get('CONTENT_LENGTH', '0'))
+        except ValueError:
+            length = 0
+
+        if length != 0:
+            body = environ['wsgi.input'].read(length)
+
+        data = {}
+        data['body'] = body
+
+        if self.environ.get('CONTENT_TYPE', '').startswith('multipart'):
+            data['files'] = True
+
+        return tools.log_json(body, 4)
--- sandbox/post_data.py
+++ sandbox/post_data.py
@@ -8,3 +8,4 @@ requests.post('http://localhost:8080', data={u'post': 'niño coraçao'})
 
 # import urllib
 # s = urllib.unquote_plus(b"ni%C3%B1o+cora%C3%A7ao").decode('utf-8')
+# print s
