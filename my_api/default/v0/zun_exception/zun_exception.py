c43a982a8d3871f2782b3f67aa35db489b6c09b0
nbari <nbari@dalmp.com>
2014-01-28 09:59:49 +0000

    modified:   zun_exception.py

--- my_api/default/v0/zun_exception/zun_exception.py
+++ my_api/default/v0/zun_exception/zun_exception.py
@@ -4,15 +4,12 @@ test HTTP exceptions
 import json
 import logging
 from zunzuncito import tools
-from zunzuncito import http_status_codes
 
 
 class APIResource(object):
 
     def __init__(self, api):
         self.api = api
-        self.status = 200
-        self.headers = api.headers.copy()
         self.log = logging.getLogger()
         self.log.info(tools.log_json({
             'vroot': api.vroot,
@@ -23,7 +20,7 @@ class APIResource(object):
         )
 
     @tools.allow_methods('get')
-    def dispatch(self, environ, start_response):
+    def dispatch(self, environ):
 
         try:
             name = self.api.path[0]
@@ -37,10 +34,5 @@ class APIResource(object):
                 description='name must be foo',
                 code='my-custom-code',
                 display=True)
-
-        headers = self.api.headers
-        start_response(
-            getattr(http_status_codes, 'HTTP_%d' %
-                    self.status), list(headers.items()))
 
         return __name__
