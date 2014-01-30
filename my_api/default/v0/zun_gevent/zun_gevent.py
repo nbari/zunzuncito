2ddce28336399bffd59cc276b0e6657feb36adbf
nbari <nbari@dalmp.com>
2014-01-30 10:58:14 +0000

    modified:   zun_gevent.py

--- my_api/default/v0/zun_gevent/zun_gevent.py
+++ my_api/default/v0/zun_gevent/zun_gevent.py
@@ -3,10 +3,9 @@ default resource
 """
 import gevent
 import gevent.socket
-import json
 import logging
-from zunzuncito import http_status_codes
-from zunzuncito.tools import MethodException, HTTPException, allow_methods
+from zunzuncito import tools
+
 
 def bg_task():
     for i in range(1, 10):
@@ -24,35 +23,33 @@ class APIResource(object):
 
     def __init__(self, api):
         self.api = api
-        self.status = 200
-        self.headers = api.headers.copy()
         self.log = logging.getLogger()
-        # self.log.setLevel('DEBUG')
-        self.log.setLevel('INFO')
-        self.log = logging.LoggerAdapter(
-            logging.getLogger(), {
-                'rid': api.request_id,
-                'indent': 4
-            })
-        self.log.info(dict((x, y) for x, y in (
-            ('API', api.version),
-            ('URI', api.URI),
-            ('method', api.method)
-        )))
+        self.log.info(tools.log_json({
+            'vroot': api.vroot,
+            'API': api.version,
+            'URI': api.URI,
+            'method': api.method
+        }, True)
+        )
 
+    def dispatch(self, environ):
 
-    def dispatch(self, environ, start_response):
-        start_response(
-            getattr(http_status_codes, 'HTTP_%d' %
-                    self.status), list(self.headers.items()))
+        self.api.headers['Content-Type'] = 'text/html; charset=UTF-8'
+
         t = gevent.spawn(long_task)
         t.join()
+
         yield "sleeping for 3 seconds...<br/>"
         gevent.sleep(3)
         yield "done<br>"
         yield "getting some ips...<br/>"
 
-        urls = ['www.google.com', 'www.example.com', 'www.python.org', 'projects.unbit.it']
+        urls = [
+            'www.google.com',
+            'www.example.com',
+            'www.python.org',
+            'projects.unbit.it']
+
         jobs = [gevent.spawn(gevent.socket.gethostbyname, url) for url in urls]
         gevent.joinall(jobs, timeout=2)
 
