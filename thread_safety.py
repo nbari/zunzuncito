ce3f5485cb70836af8ebff79c73e228f469ca99d
nbari <nbari@dalmp.com>
2014-02-08 11:32:01 +0000

    modified:   thread_safety.py

--- thread_safety.py
+++ thread_safety.py
@@ -6,23 +6,22 @@ import zunzuncito
 root = 'my_api'
 app = zunzuncito.ZunZun(root, rid='rid', debug=True)
 
-environ = {
-    'rid': str(uuid4()),
-    'REQUEST_URI': '/env',
-    'REQUEST_METHOD': 'get'
-}
-
 
 def start_response(status, headers):
     print status, headers
 
 
 def fake_req(num):
-    environ.update({'thread': num})
+    environ = {
+        'rid': str(uuid4()),
+        'REQUEST_URI': '/env',
+        'REQUEST_METHOD': 'get',
+        'thread': num
+    }
     print app(environ, start_response)
 
 threads = []
-for i in range(1):
+for i in range(2):
     threads.append(Thread(target=fake_req, args=(i,)))
 for t in threads:
     t.start()
