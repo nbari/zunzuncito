0093ecefc1ffcd5137a7e5b96404debc1c8f75af
nbari <nbari@dalmp.com>
2014-02-18 08:34:25 +0000

    new file:   my_api/beta_zunzun_io/v0/zun_default/__init__.py 	new file:   my_api/beta_zunzun_io/v0/zun_default/zun_default.py

---
+++ my_api/beta_zunzun_io/v0/zun_default/zun_default.py
@@ -0,0 +1,29 @@
+"""
+default resource
+"""
+from zunzuncito import tools
+
+
+class APIResource(object):
+
+    def dispatch(self, request, response):
+
+        request.log.debug(tools.log_json({
+            'API': request.version,
+            'URI': request.URI,
+            'method': request.method,
+            'vroot': request.vroot
+        }, True))
+
+        data = {}
+        data['about'] = "Hi %s, I am %s on %s" % (
+            request.environ.get('REMOTE_ADDR',
+                                0),
+            request.host,
+            request.vroot)
+
+        data['Request-ID'] = request.request_id
+        data['URI'] = request.URI
+        data['Method'] = request.method
+
+        return tools.log_json(data, 4)
