b42fb722a0794348a7db56b23dd454c0501edcb4
nbari <nbari@dalmp.com>
2014-02-23 08:56:35 +0000

    modified:   my_api/default/v1/zun_env/zun_env.py

--- my_api/default/v1/zun_env/zun_env.py
+++ my_api/default/v1/zun_env/zun_env.py
@@ -1,30 +1,12 @@
 """
-print the environ dict
+v1 print the environ dict
 """
-
-import logging
 
 from zunzuncito import tools
 
 
 class APIResource(object):
 
-    def __init__(self, req):
-        self.req = req
-        self.log = req.log
-        self.log.debug(tools.log_json({
-            'API': req.version,
-            'URI': req.URI,
-            'rid': req.request_id,
-            'vroot': req.vroot
-        }, True)
-        )
+    def dispatch(self, request, response):
 
-    def __call__(self, req):
-        self.req = req
-
-    def dispatch(self, environ):
-
-        self.log.info(tools.log_json({'rid': self.req.request_id}))
-
-        return tools.log_json(environ, 4)
+        return tools.log_json(request.environ, 4)
