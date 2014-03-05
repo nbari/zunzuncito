3a70c3ce960bfc51bbd0ffd7b23ca91362007492
nbari <nbari@dalmp.com>
2014-03-05 13:50:37 +0000

    new file:   my_api/default/v0/zun_ua/__init__.py 	new file:   my_api/default/v0/zun_ua/zun_ua.py

---
+++ my_api/default/v0/zun_ua/zun_ua.py
@@ -0,0 +1,19 @@
+"""
+print the environ dict
+"""
+
+from zunzuncito import tools
+
+
+class APIResource(object):
+
+    def dispatch(self, request, response):
+
+        request.log.debug(tools.log_json({
+            'API': request.version,
+            'Method': request.method,
+            'URI': request.URI,
+            'vroot': request.vroot
+        }, True))
+
+        return request.environ.get('HTTP_USER_AGENT').encode('utf-8')
