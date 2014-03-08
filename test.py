64760b5686bea4ae6fb3f294da418567cd29c67e
nbari <nbari@dalmp.com>
2014-03-08 12:30:41 +0000

    new file:   my_api/default/v0/zun_test/__init__.py 	new file:   my_api/default/v0/zun_test/zun_test.py 	modified:   test.py

--- test.py
+++ test.py
@@ -1,39 +1,121 @@
+"""
+python text.py
+"""
+
+import sys
+import time
 import unittest
 import zunzuncito
+
+from threading import Thread
+from uuid import uuid1
+from zunzuncito import tools
 
 
 class ZunZunTest(unittest.TestCase):
     versions = ['  ', '', 1, 'v0', 'my version', (1,), 'api/v1']
 
-    routes = [
-        ('/.*', 'default'),
-        ('/teste', 'test_put', 'PUT'),
-        ('^/teste', 'test_put', 'PUT'),
-        ('[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}'),
-        ('/?')
-    ]
+    hosts = {
+        '*': 'default',
+        'api.zunzun.io': 'default',
+        'beta.zunzun.io': 'beta_zunzun_io',
+        'oauth.zunzun.io': 'oauth',
+        'oic.zunzun.io': 'oic'
+    }
+
+    routes = {
+        'default':
+        [
+            ('/.*', 'default'),
+            ('/teste', 'test_put', 'PUT'),
+            ('^/teste', 'test_put', 'PUT'),
+            ('[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}',
+             'uuid'),
+            ('/?', 'xxx'),
+            ('/.*', 'default')
+        ],
+        'other':
+        [
+            (r'/(.*\.(gif|png|jpg|ico|bmp|css|otf|woff))', 'static')
+        ]
+    }
 
     def test_versions(self):
-        zun = zunzuncito.ZunZun('/tmp', self.versions)
-        self.assertEqual(5, len(zun.versions))
+        app = zunzuncito.ZunZun('/tmp', self.versions)
+        self.assertEqual(5, len(app.versions))
+
+    def test_hosts(self):
+        app = zunzuncito.ZunZun('/tmp', self.versions, self.hosts)
+        self.assertEqual(len(self.hosts), len(app.hosts))
 
     def test_register_routes(self):
-        zun = zunzuncito.ZunZun('/tmp', self.versions, self.routes)
-        zun_routes = zun.routes
+        app = zunzuncito.ZunZun('/tmp', routes=self.routes)
 
-        for regex, pattern in zip(self.routes, zun_routes):
-            # print regex[0], pattern[0].pattern
-            if regex[0].startswith('^'):
-                if regex[0] != pattern[0].pattern:
-                    self.assertTrue(False)
-                    break
-            else:
-                if '^%s$' % regex[0] != pattern[0].pattern:
-                    self.assertTrue(False)
-                    break
+        for k in self.routes.keys():
+            self.assertEqual(len(self.routes[k]), len(app.routes[k]))
+            for regex, pattern in zip(self.routes[k], app.routes[k]):
+                # print regex[0], pattern[0].pattern
+                if regex[0].startswith('^'):
+                    if regex[0] != pattern[0].pattern:
+                        self.assertTrue(False)
+                        break
+                else:
+                    if '^%s$' % regex[0] != pattern[0].pattern:
+                        self.assertTrue(False)
+                        break
 
         self.assertTrue(True)
 
+    def test_rid_unique(self):
+        app = zunzuncito.ZunZun('my_api', rid='rid')
+        out = []
+
+        def start_response(status, headers):
+            self.assertFalse(headers[0][1] in out)
+            out.append(headers[0][1])
+
+        for environ in fake_req(1000):
+            body = app(environ, start_response)
+
+        self.assertEqual(1000, len(out))
+
+    def test_Thread_safety(self):
+        app = zunzuncito.ZunZun('my_api', rid='rid')
+        out = []
+
+        def start_response(status, headers):
+            self.assertFalse(headers[0][1] in out)
+            out.append(headers[0][1])
+
+        def make_req(times):
+            for environ in fake_req(times):
+                body = app(environ, start_response)
+
+        """ num of threads t, and num or requests r """
+        t = 4
+        r = 250
+        threads = []
+        for _ in range(t):
+            threads.append(Thread(target=make_req, args=(r,)))
+        for t in threads:
+            t.start()
+        for t in threads:
+            t.join()
+
+        """ out = threads x request t*r"""
+        self.assertEqual(1000, len(out))
+
+
+def fake_req(num, uri='/test'):
+    i = 0
+    while i < num:
+        environ = {
+            'rid': str(uuid1()),
+            'REQUEST_URI': uri,
+            'REQUEST_METHOD': 'get'
+        }
+        yield environ
+        i += 1
 
 if __name__ == '__main__':
     unittest.main()
