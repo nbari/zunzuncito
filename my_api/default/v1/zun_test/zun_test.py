d0332558c272e497539d3642405c1305c2fd63c7
nbari <nbari@dalmp.com>
2014-03-08 15:05:29 +0000

    new file:   my_api/default/v1/zun_test/__init__.py 	new file:   my_api/default/v1/zun_test/zun_test.py 	modified:   test.py

---
+++ my_api/default/v1/zun_test/zun_test.py
@@ -0,0 +1,22 @@
+"""
+/test resource
+"""
+
+from zunzuncito import tools
+
+
+class APIResource(object):
+
+    def dispatch(self, request, response):
+
+        request.log.debug(tools.log_json({
+            'API': request.version,
+            'Method': request.method,
+            'URI': request.URI,
+            'vroot': request.vroot
+        }, True))
+
+        if request.path:
+            response.status = int(request.path[0])
+
+        return response.request_id
--- test.py
+++ test.py
@@ -20,7 +20,7 @@ def py_version(f):
 
 
 class ZunZunTest(unittest.TestCase):
-    versions = ['  ', '', 1, 'v0', 'my version', (1,), 'api/v1']
+    versions = ['  ', '', 1, 'v0', 'my version', (1,), 'api/v1', 'v1']
 
     hosts = {
         '*': 'default',
@@ -49,7 +49,8 @@ class ZunZunTest(unittest.TestCase):
 
     def test_versions(self):
         app = zunzuncito.ZunZun('/tmp', self.versions)
-        self.assertEqual(5, len(app.versions))
+        print app.versions
+        self.assertEqual(6, len(app.versions))
 
     def test_hosts(self):
         app = zunzuncito.ZunZun('/tmp', self.versions, self.hosts)
@@ -116,6 +117,32 @@ class ZunZunTest(unittest.TestCase):
 
         """ check if list are equals """
         self.assertTrue(set(out) == set(body))
+
+    def test_status_200(self):
+        app = zunzuncito.ZunZun('my_api')
+
+        def start_response(status, headers):
+            self.assertTrue(status.startswith('200'))
+
+        environ = list(fake_req(1, '/test'))
+        body = app(environ[0], start_response)
+
+    def test_status_xxx(self):
+        app = zunzuncito.ZunZun('my_api', self.versions)
+
+        for i in range(100, 600, 100):
+            def start_response(status, headers):
+                self.assertTrue(status.startswith(str(i)))
+
+            environ = list(fake_req(1, '/v1/test/%s' % i))
+            body = app(environ[0], start_response)
+
+        for i in range(100, 600, 99):
+            def start_response(status, headers):
+                self.assertTrue(status.startswith(str((i // 100) * 100)))
+
+            environ = list(fake_req(1, '/v1/test/%s' % i))
+            body = app(environ[0], start_response)
 
 
 def fake_req(num, uri='/test'):
