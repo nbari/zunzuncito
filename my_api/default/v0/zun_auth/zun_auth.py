19cf7556a64c9f7a388e225e773fbf0820251d87
nbari <nbari@dalmp.com>
2014-03-19 11:41:32 +0000

    new file:   my_api/default/v0/zun_auth/__init__.py 	new file:   my_api/default/v0/zun_auth/zun_auth.py

---
+++ my_api/default/v0/zun_auth/zun_auth.py
@@ -0,0 +1,21 @@
+"""
+/auth  resource
+
+http://en.wikipedia.org/wiki/Basic_access_authentication
+"""
+
+from zunzuncito import tools
+
+
+class APIResource(object):
+
+    def dispatch(self, request, response):
+
+        request.log.debug(tools.log_json({
+            'API': request.version,
+            'Method': request.method,
+            'URI': request.URI,
+            'vroot': request.vroot
+        }, True))
+
+        return tools.log_json(request.environ, 4)
